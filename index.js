f0062dd944806e3bbedf680e642787b827f2214e
Manasseh Amoako <xasongab@gmail.com>
2025-04-20 21:03:25 +0100

    Fix: Serve assets directory

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
+const _path = process.cwd();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 4000;
+
+// Serve static files
+app.use('/assets', express.static(__dirname + '/assets'));
+
 let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
+
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+app.use('/pair', async (req, res, next) => {
+  res.sendFile(_path + '/pair.html');
+});
+app.use('/', async (req, res, next) => {
+  res.sendFile(_path + '/main.html');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log('Server is running! Powered by Xason XtarmD');
-})
 
-module.exports = app
+app.listen(PORT, () => {
+  console.log('Server is running! Powered by Xason XtarmD');
+});
+
+module.exports = app;
